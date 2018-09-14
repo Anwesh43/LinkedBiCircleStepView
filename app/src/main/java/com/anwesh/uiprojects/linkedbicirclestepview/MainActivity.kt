104f65b08ed76daff00dfcaa3fe118010b54ef93
Anwesh43 <anweshthecool0@gmail.com>
2018-09-14 10:57:52 +0530

    adding BiCircleStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbicirclestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbicirclestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbicirclestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bicirclestepview.BiCircleStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiCircleStepView.create(this)
     }
 }
