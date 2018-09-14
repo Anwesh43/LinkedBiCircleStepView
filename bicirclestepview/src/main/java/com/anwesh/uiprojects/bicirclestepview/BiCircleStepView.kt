d984335b6f24cf770243b8b995a922a0924ed255
Anwesh43 <anweshthecool0@gmail.com>
2018-09-14 10:12:04 +0530

    adding libraries and defining nodes

--- bicirclestepview/src/main/java/com/anwesh/uiprojects/bicirclestepview/BiCircleStepView.kt
+++ bicirclestepview/src/main/java/com/anwesh/uiprojects/bicirclestepview/BiCircleStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.bicirclestepview
 /**
  * Created by anweshmishra on 14/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+
