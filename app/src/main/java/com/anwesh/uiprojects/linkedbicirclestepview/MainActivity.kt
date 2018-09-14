52ea9d187e92fe7b3336bead31b62a0d553837ab
Anwesh43 <anweshthecool0@gmail.com>
2018-09-14 11:04:41 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbicirclestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbicirclestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedbicirclestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.bicirclestepview.BiCircleStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BiCircleStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
